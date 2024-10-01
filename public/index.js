83420ebec486f8b3c5e3c4faf684aad93ed4304a
German Fica <germafica@gmail.com>
2024-10-01 10:47:55 -0300

    Add animation progress

--- public/index.js
+++ public/index.js
@@ -2,3 +2,5 @@
 import '@material/web/button/filled-button.js';
 import '@material/web/button/outlined-button.js';
 import '@material/web/checkbox/checkbox.js';
+import '@material/web/progress/linear-progress.js';
+import '@material/web/progress/circular-progress.js';
